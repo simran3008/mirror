c431d7aa60678b448aeee0e948cfd83970dcada0
simranjaising <skj2260@rit.edu>
2018-09-16 11:35:50 -0700

    Revert "Updating actions, reducers etc.."
    This reverts commit de82760e64334e7b778c1c9b4e1cc750e6657ec7.

--- client/index.js
+++ client/index.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import mirrorReducers from '../reducers'
-import helloWorld from '../actions'
 import App from '../Components/App/App.jsx';
-import {
-  connect
-}
-from 'react-redux'
 
-const HelloWorld = connect(
-  mirrorReducers,
-  helloWorld
-)(App)
-
-export default HelloWorld
+ReactDOM.render(<App />, document.getElementById('root'));
